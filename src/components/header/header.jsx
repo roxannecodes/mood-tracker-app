4a0f55e21d40370d39f2711d8107ec7d4c55f8f2
Roxanne <roxanne.shoar@gmail.com>
2025-12-12 02:12:16 -0500

    refactor: header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,10 @@
 import "./header.css";
 
-export default function Header({ onLogMoodClick, onStatsClick }) {
+export default function Header() {
   return (
     <header className="app-header">
       <h1>HoliMood</h1>
-      <nav>
-        <button className="log-mood-btn" onClick={onLogMoodClick}>
-          + Log Mood
-        </button>
-        <button className="log-mood-btn" onClick={onStatsClick}>
-          View Stats
-        </button>
-      </nav>
+      
     </header>
   );
 }
