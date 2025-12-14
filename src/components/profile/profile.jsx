6c2833507260714753c4255ce92d044dae5c2023
Hunter Melrose <huntermelrose831@gmail.com>
2025-12-13 18:28:48 -0800

    feat: css

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,11 +1,9 @@
 import "./profile.css";
-
+import Avatar from "../../assets/Avatar.jpg";
 export default function Profile({ onLogMoodClick, onStatsClick }) {
   // You can later make these dynamic from user data
   const userName = "Eve Smith";
-  const userAvatar =
-    "https://testingbot.com/free-online-tools/random-avatar/200?img=1";
-
+  const userAvatar = Avatar;
   const handleEditProfile = () => {
     console.log("Edit profile clicked");
     // TODO: Open edit profile modal
@@ -16,16 +14,14 @@ export default function Profile({ onLogMoodClick, onStatsClick }) {
       <div className="profile__left">
         <img src={userAvatar} alt={userName} className="profile__avatar" />
         <h2 className="profile__name">{userName}</h2>
-        <button className="profile__edit-button" onClick={handleEditProfile}>
-          Edit Profile
-        </button>
+        <button
+          className="profile__edit-button"
+          onClick={handleEditProfile}
+        ></button>
       </div>
 
       <div className="profile__right">
-        <button
-          className="profile__button profile__button--primary"
-          onClick={onLogMoodClick}
-        >
+        <button className="profile__button" onClick={onLogMoodClick}>
           + Log Mood
         </button>
         <button className="profile__button" onClick={onStatsClick}>
